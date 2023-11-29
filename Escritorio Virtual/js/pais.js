9d9d269a5c64144a07a1088d04c53af7814b4db1
Veneiro <veneiroyt@gmail.com>
2023-11-29 19:51:45 +0100

    started weathercast

--- Escritorio Virtual/js/pais.js
+++ Escritorio Virtual/js/pais.js
@@ -1,35 +1,160 @@
-class Pais{
-    constructor(nombre, capital, poblacion){
-        this.nombre = nombre;
-        this.capital = capital;
-        this.poblacion = poblacion;
-    }
+("use strict");
+class Pais {
+  constructor(nombre, capital, poblacion) {
+    this.nombre = nombre;
+    this.capital = capital;
+    this.poblacion = poblacion;
 
-    setAtributosSecundarios(gobierno, coordCapital, religion){
-        this.gobierno = gobierno;
-        this.coordCapital = coordCapital;
-        this.religion = religion;
-    }
+    this.apikey = "ff03cf1de5d47b151a031ab0ff83134d";
+    this.ciudad = "Oviedo";
+    this.codigoPais = "ES";
+    this.unidades = "&units=metric";
+    this.idioma = "&lang=es";
+    this.cnt = 5;
+    this.url =
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      this.ciudad +
+      "," +
+      this.codigoPais +
+      "&cnt=" +
+      this.cnt +
+      "&units=" +
+      this.unidades +
+      "&lang=" +
+      this.idioma +
+      "&APPID=" +
+      this.apikey;
+  }
 
-    getNombreYCapital(){
-        return this.nombre + " " + this.capital;
-    }
+  setAtributosSecundarios(gobierno, coordCapital, religion) {
+    this.gobierno = gobierno;
+    this.coordCapital = coordCapital;
+    this.religion = religion;
+  }
 
-    getAtributosSecundarios(){
-        var attrb = "";
-        attrb += "<p> Poblacion: " + this.poblacion + "<\p>";
-        attrb += "<p> Gobierno: " + this.gobierno + "<\p>";
-        attrb += "<p> Religión: " + this.religion + "<\p>";
-        return attrb;
-    }
+  getNombreYCapital() {
+    return this.nombre + " " + this.capital;
+  }
 
-    insertCoordCapital(){
-        document.write("<p>" + this.coordCapital + "</p>");
-    }
+  getAtributosSecundarios() {
+    var attrb = "";
+    attrb += "<p> Poblacion: " + this.poblacion + "<p>";
+    attrb += "<p> Gobierno: " + this.gobierno + "<p>";
+    attrb += "<p> Religión: " + this.religion + "<p>";
+    return attrb;
+  }
+
+  insertCoordCapital() {
+    document.write("<p>" + this.coordCapital + "</p>");
+  }
+
+  cargarDatos() {
+    $.ajax({
+      dataType: "json",
+      url: this.url,
+      method: "GET",
+      success: function (datos) {
+        $("pre").text(JSON.stringify(datos, null, 2)); //muestra el json en un elemento pre
+        console.log(this.url)
+        console.log(datos)
+        /**
+        //PresentaciÃ³n de los datos contenidos en JSON
+        var stringDatos = "<h2>Pronóstico para 5 días</h2>";
+        stringDatos += "<ul><li>Ciudad: " + datos.name + "</li>";
+        stringDatos += "<li>PaÃ­s: " + datos.sys.country + "</li>";
+        stringDatos += "<li>Latitud: " + datos.coord.lat + " grados</li>";
+        stringDatos += "<li>Longitud: " + datos.coord.lon + " grados</li>";
+        stringDatos +=
+          "<li>Temperatura: " + datos.main.temp + " grados Celsius</li>";
+        stringDatos +=
+          "<li>Temperatura mÃ¡xima: " +
+          datos.main.temp_max +
+          " grados Celsius</li>";
+        stringDatos +=
+          "<li>Temperatura mÃ­nima: " +
+          datos.main.temp_min +
+          " grados Celsius</li>";
+        stringDatos +=
+          "<li>PresiÃ³n: " + datos.main.pressure + " milibares</li>";
+        stringDatos += "<li>Humedad: " + datos.main.humidity + " %</li>";
+        stringDatos +=
+          "<li>Amanece a las: " +
+          new Date(datos.sys.sunrise * 1000).toLocaleTimeString() +
+          "</li>";
+        stringDatos +=
+          "<li>Oscurece a las: " +
+          new Date(datos.sys.sunset * 1000).toLocaleTimeString() +
+          "</li>";
+        stringDatos +=
+          "<li>DirecciÃ³n del viento: " + datos.wind.deg + " grados</li>";
+        stringDatos +=
+          "<li>Velocidad del viento: " +
+          datos.wind.speed +
+          " metros/segundo</li>";
+        stringDatos +=
+          "<li>Hora de la medida: " +
+          new Date(datos.dt * 1000).toLocaleTimeString() +
+          "</li>";
+        stringDatos +=
+          "<li>Fecha de la medida: " +
+          new Date(datos.dt * 1000).toLocaleDateString() +
+          "</li>";
+        stringDatos +=
+          "<li>DescripciÃ³n: " + datos.weather[0].description + "</li>";
+        stringDatos += "<li>Visibilidad: " + datos.visibility + " metros</li>";
+        stringDatos += "<li>Nubosidad: " + datos.clouds.all + " %</li></ul>";
+        pais.crearElemento("p",stringDatos, "body");
+         */
+      },
+      error: function () {
+        $("h3").html(
+          "Â¡Tenemos problemas! No puedo obtener JSON de <a href='http://openweathermap.org'>OpenWeatherMap</a>"
+        );
+        $("h4").remove();
+        $("pre").remove();
+        $("p").remove();
+      },
+    });
+  }
+
+  crearElemento(tipoElemento, texto, insertarAntesDe) {
+    // Crea un nuevo elemento modificando el Ã¡rbol DOM
+    // El elemnto creado es de 'tipoElemento' con un 'texto'
+    // El elemnto se coloca antes del elemnto 'insertarAntesDe'
+    var elemento = document.createElement(tipoElemento);
+    elemento.innerHTML = texto;
+    $(insertarAntesDe).append(elemento);
+  }
+
+  verJSON() {
+    //Muestra el archivo JSON recibido
+    this.crearElemento(
+      "h2",
+      "Datos en JSON desde <a href='http://openweathermap.org'>OpenWeatherMap</a>",
+      "main"
+    );
+    this.crearElemento("h3", this.correcto, "main"); // Crea un elemento con DOM
+    this.crearElemento("h4", "JSON", "main"); // Crea un elemento con DOM
+    this.crearElemento("pre", "", "main"); // Crea un elemento con DOM para el string con JSON
+    this.crearElemento("h4", "Datos", "main"); // Crea un elemento con DOM
+    this.crearElemento("p", "", "main"); // Crea un elemento con DOM para los datos obtenidos con JSON
+    this.cargarDatos();
+    $("button").attr("disabled", "disabled");
+  }
 }
 
 var pais = new Pais("Belgica", "Bruselas", 11590000);
-pais.setAtributosSecundarios("Monarquía Federal Parlamentaria", "50.846667, 4.351667", "Catolicismo");
-document.write("<p>" + pais.getNombreYCapital() + "</p>");
-document.write("<article>" + pais.getAtributosSecundarios() + "</article>");
+pais.setAtributosSecundarios(
+"Monarquía Federal Parlamentaria",
+"50.846667, 4.351667",
+"Catolicismo"
+);
+pais.crearElemento("p",  pais.getNombreYCapital(), "body");
+pais.crearElemento("article", pais.getAtributosSecundarios(), "body");
+
+pais.cargarDatos();
+//document.write("<p>" + pais.getNombreYCapital() + "</p>");
+//document.write("<article>" + pais.getAtributosSecundarios() + "</article>");
 pais.insertCoordCapital();
+
+
--- Escritorio Virtual/js/sudoku.js
+++ Escritorio Virtual/js/sudoku.js
@@ -1,102 +1,147 @@
 class Sudoku {
-
-    constructor() {
-        this.rows = 9;
-        this.columns = 9;
-        this.tablero = "3.4.69.5....27...49.2..4....2..85.198.9...2.551.39..6....8..5.32...46....4.75.9.6";
-        this.tableroArray = new Array(this.rows);
-        this.keySelected = null;
-        for (let i = 0; i < this.rows; i++) {
-            this.tableroArray[i] = new Array(this.columns);
-        }
+  constructor() {
+    this.rows = 9;
+    this.columns = 9;
+    this.tablero =
+      "3.4.69.5....27...49.2..4....2..85.198.9...2.551.39..6....8..5.32...46....4.75.9.6";
+    this.tableroArray = new Array(this.rows);
+    this.keySelected = null;
+    for (let i = 0; i < this.rows; i++) {
+      this.tableroArray[i] = new Array(this.columns);
     }
+  }
 
-    start() {
-        document.querySelector('body').addEventListener('keydown', this.select.bind(this));
-        for (let i = 0; i < this.tableroArray.length; i++) {
-            for (let j = 0; j < this.tableroArray[i].length; j++) {
-                // Acceder a cada elemento de la matriz
-                if (this.tablero.charAt(i * this.columns + j) == '.') {
-                    this.tableroArray[i][j] = 0;
-                } else {
-                    this.tableroArray[i][j] = this.tablero.charAt(i * this.columns + j);
-                }
+  start() {
+    document
+      .querySelector("body")
+      .addEventListener("keydown", this.select.bind(this));
+    for (let i = 0; i < this.tableroArray.length; i++) {
+      for (let j = 0; j < this.tableroArray[i].length; j++) {
+        // Acceder a cada elemento de la matriz
+        if (this.tablero.charAt(i * this.columns + j) == ".") {
+          this.tableroArray[i][j] = 0;
+        } else {
+          this.tableroArray[i][j] = this.tablero.charAt(i * this.columns + j);
+        }
+      }
+    }
+  }
+
+  createStructure() {
+    const section = document.querySelector("main");
+    for (let i = 0; i < this.tableroArray.length; i++) {
+      for (let j = 0; j < this.tableroArray[i].length; j++) {
+        const a = document.createElement("p");
+        if (this.tableroArray[i][j] == 0) {
+          a.innerHTML = "";
+          a.setAttribute("data-state", " ");
+          a.addEventListener("click", () => {
+            if (a.getAttribute("data-state") == " ") {
+              if (this.keySelected != null) {
+                this.keySelected.setAttribute("data-state", " ");
+              }
+              this.keySelected = a;
+              a.setAttribute("data-state", "clicked");
             }
+          });
+        } else {
+          a.innerHTML = this.tableroArray[i][j];
+          a.setAttribute("data-state", "blocked");
         }
+        section.append(a);
+      }
     }
+  }
 
-    createStructure() {
-        const section = document.querySelector('main');
-        for (let i = 0; i < this.tableroArray.length; i++) {
-            for (let j = 0; j < this.tableroArray[i].length; j++) {
-                const a = document.createElement('p');
-                if (this.tableroArray[i][j] == 0) {
-                    a.innerHTML = "";
-                    a.setAttribute('data-state', ' ');
-                    a.addEventListener('click', () => {
-                        if (a.getAttribute('data-state') == ' ') {
-                            if (this.keySelected != null) {
-                                this.keySelected.setAttribute('data-state', ' ');
-                            }
-                            this.keySelected = a
-                            a.setAttribute('data-state', 'clicked');
-                        }
-                    });
-                } else {
-                    a.innerHTML = this.tableroArray[i][j];
-                    a.setAttribute('data-state', 'blocked');
-                }
-                section.append(a);
-            }
-        }
+  select(event) {
+    const tecla = event.key;
+    if (this.keySelected != null) {
+      switch (tecla) {
+        case "1":
+          this.introduceNumber(1);
+          break;
+        case "2":
+          this.introduceNumber(2);
+          break;
+        case "3":
+          this.introduceNumber(3);
+          break;
+        case "4":
+          this.introduceNumber(4);
+          break;
+        case "5":
+          this.introduceNumber(5);
+          break;
+        case "6":
+          this.introduceNumber(6);
+          break;
+        case "7":
+          this.introduceNumber(7);
+          break;
+        case "8":
+          this.introduceNumber(8);
+          break;
+        case "9":
+          this.introduceNumber(9);
+          break;
+      }
     }
+  }
 
-    select(event) {
-        const tecla = event.key;
-        if (this.keySelected != null) {
-            switch (tecla) {
-                case "1":
-                    this.introduceNumber(1);
-                    break;
-                case "2":
-                    this.introduceNumber(2);
-                    break;
-                case "3":
-                    this.introduceNumber(3);
-                    break;
-                case "4":
-                    this.introduceNumber(4);
-                    break;
-                case "5":
-                    this.introduceNumber(5);
-                    break;
-                case "6":
-                    this.introduceNumber(6);
-                    break;
-                case "7":
-                    this.introduceNumber(7);
-                    break;
-                case "8":
-                    this.introduceNumber(8);
-                    break;
-                case "9":
-                    this.introduceNumber(9);
-                    break;
-            }
-        }
-    }
+  introduceNumber(number) {
+    if (this.keySelected.getAttribute("data-state") == "clicked") {
+      const section = document.querySelector("main");
+      const selectedIndex = Array.from(section.children).indexOf(
+        this.keySelected
+      );
 
-
-
-    introduceNumber(number) {
+      const rowIndex = Math.floor(selectedIndex / this.columns);
+      const colIndex = selectedIndex % this.columns;
+      if (
+        !this.checkRow(rowIndex, number) &&
+        !this.checkColumn(colIndex, number) &&
+        !this.checkSubgrid(rowIndex, colIndex, number)
+      ) {
         this.keySelected.innerHTML = number;
-        this.keySelected.setAttribute('data-state', 'correct')
-        this.keySelected.removeEventListener('click');
+        this.keySelected.setAttribute("data-state", "correct");
+      } else {
+        // Mensaje o acción cuando se violan las reglas
+        alert("Número no válido en esta posición.");
+      }
     }
+  }
 
-    paintSudoku() {
-        this.createStructure();
+  checkRow(rowIndex, number) {
+    return this.tableroArray[rowIndex].some((cell) => cell == number);
+  }
+
+  checkColumn(colIndex, number) {
+    for (let i = 0; i < this.rows; i++) {
+      if (this.tableroArray[i][colIndex] == number) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  checkSubgrid(rowIndex, colIndex, number) {
+    const subgridSize = 3;
+    const startRow = Math.floor(rowIndex / subgridSize) * subgridSize;
+    const startCol = Math.floor(colIndex / subgridSize) * subgridSize;
+
+    for (let i = 0; i < subgridSize; i++) {
+      for (let j = 0; j < subgridSize; j++) {
+        if (this.tableroArray[startRow + i][startCol + j] == number) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  paintSudoku() {
+    this.createStructure();
+  }
 }
 
 const sudoku = new Sudoku();
